6d36dbdf6207961b445d16e39b646aaf85d7c8a1
Taylor Hanayik <hanayik@gmail.com>
2024-07-15 21:01:02 +0100

    refactor and make things run

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { Niivue } from '@niivue/niivue'
-import * as ort from 'onnxruntime-web';
+// IMPORTANT: we need to import this specific file. 
+import * as ort from "./node_modules/onnxruntime-web/dist/ort.all.mjs"
+console.log(ort);
 async function main() {
   aboutBtn.onclick = function () {
     let url = "https://github.com/axinging/mlmodel-convension-demo/blob/main/onnx/onnx-brainchop.html"
@@ -17,79 +19,103 @@ async function main() {
   const nv1 = new Niivue(defaults)
   nv1.attachToCanvas(gl1)
   await nv1.loadVolumes([{ url: './t1_crop.nii.gz' }])
+  // FIXME: Do we want to conform?
+  const conformed = await nv1.conform(
+    nv1.volumes[0],
+    false,
+    true,
+    true
+  )
+  nv1.removeVolume(nv1.volumes[0])
+  nv1.addVolume(conformed)
 
   let feedsInfo = [];
   function getFeedInfo(feed, type, data, dims) {
-      const warmupTimes = 0;
-      const runTimes = 1;
-      for (let i = 0; i < warmupTimes + runTimes; i++) {
-          let typedArray;
-          let typeBytes;
-          if (type === 'bool') {
-              data = [data];
-              dims = [1];
-              typeBytes = 1;
-          } else if (type === 'int8') {
-              typedArray = Int8Array;
-          } else if (type === 'float16') {
-              typedArray = Uint16Array;
-          } else if (type === 'int32') {
-              typedArray = Int32Array;
-          } else if (type === 'uint32') {
-              typedArray = Uint32Array;
-          } else if (type === 'float32') {
-              typedArray = Float32Array;
-          } else if (type === 'int64') {
-              typedArray = BigInt64Array;
-          }
-          if (typeBytes === undefined) {
-              typeBytes = typedArray.BYTES_PER_ELEMENT;
-          }
-
-          let size, _data;
-          if (Array.isArray(data) || ArrayBuffer.isView(data)) {
-              size = data.length;
-              _data = data;
-          } else {
-              size = dims.reduce((a, b) => a * b);
-              if (data === 'random') {
-                  _data = typedArray.from({ length: size }, () => getRandom(type));
-              } else {
-                  _data = typedArray.from({ length: size }, () => data);
-              }
-          }
-
-          if (i > feedsInfo.length - 1) {
-              feedsInfo.push(new Map());
-          }
-          feedsInfo[i].set(feed, [type, _data, dims, Math.ceil(size * typeBytes / 16) * 16]);
+    const warmupTimes = 0;
+    const runTimes = 1;
+    for (let i = 0; i < warmupTimes + runTimes; i++) {
+      let typedArray;
+      let typeBytes;
+      if (type === 'bool') {
+        data = [data];
+        dims = [1];
+        typeBytes = 1;
+      } else if (type === 'int8') {
+        typedArray = Int8Array;
+      } else if (type === 'float16') {
+        typedArray = Uint16Array;
+      } else if (type === 'int32') {
+        typedArray = Int32Array;
+      } else if (type === 'uint32') {
+        typedArray = Uint32Array;
+      } else if (type === 'float32') {
+        typedArray = Float32Array;
+      } else if (type === 'int64') {
+        typedArray = BigInt64Array;
       }
-      return feedsInfo;
+      if (typeBytes === undefined) {
+        typeBytes = typedArray.BYTES_PER_ELEMENT;
+      }
+
+      let size, _data;
+      if (Array.isArray(data) || ArrayBuffer.isView(data)) {
+        size = data.length;
+        _data = data;
+      } else {
+        size = dims.reduce((a, b) => a * b);
+        if (data === 'random') {
+          _data = typedArray.from({ length: size }, () => getRandom(type));
+        } else {
+          _data = typedArray.from({ length: size }, () => data);
+        }
+      }
+
+      if (i > feedsInfo.length - 1) {
+        feedsInfo.push(new Map());
+      }
+      feedsInfo[i].set(feed, [type, _data, dims, Math.ceil(size * typeBytes / 16) * 16]);
+    }
+    return feedsInfo;
   }
   const option = {
-      executionProviders: [
-          {
-              //name: 'webgpu',
-              name: 'webgl',
-          },
-      ],
-      graphOptimizationLevel: 'extended',
-      optimizedModelFilepath: 'opt.onnx'
+    executionProviders: [
+      {
+        name: 'webgpu',
+      },
+    ],
+    graphOptimizationLevel: 'extended',
+    optimizedModelFilepath: 'opt.onnx'
   };
 
   const session = await ort.InferenceSession.create('./model_5_channels.onnx', option);
   const shape = [1, 1, 256, 256, 256];
-  const temp = getFeedInfo("input.1", "float32", 0, shape);
+  // FIXME: Do we want to use a real image for inference?
+  const imgData = nv1.volumes[0].img;
+  const expectedLength = shape.reduce((a, b) => a * b);
+  // FIXME: Do we need want this?
+  if (imgData.length !== expectedLength) {
+    throw new Error(`imgData length (${imgData.length}) does not match expected tensor length (${expectedLength})`);
+  }
+
+  const temp = getFeedInfo("input.1", "float32", imgData, shape);
   let dataA = temp[0].get('input.1')[1];
-  // let dataTemp = await loadJSON("./onnx-branchchop-input64.jsonc");
-  // dataA = dataTemp['data'];
   const tensorA = new ort.Tensor('float32', dataA, shape);
-  
+
   const feeds = { "input.1": tensorA };
   // feed inputs and run
   console.log("before run");
   const results = await session.run(feeds);
-  console.log("after run");
+  console.log(results);
+  console.log("after run")
+  // FIXME: is this really the output data? It doesn't make sense when rendered, 
+  // but then again, maybe the input was wrong?
+  const outData = results[39].data
+  const newImg = nv1.cloneVolume(0);
+  newImg.img = outData
+  // Add the output to niivue
+  nv1.addVolume(newImg)
+  nv1.setColormap(newImg.id, "red")
+  nv1.setOpacity(1, 0.5)
 }
 
 main()
